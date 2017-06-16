6f1012af12221016d68cce30388ea28f4e942987
Bartal Clementsen <BartalClementsen@hotmail.com>
2017-06-16 22:33:54 +0100

    better moving

--- UnityProjects/BuggerBall/Assets/Entities/Player/PlayerHandler.cs
+++ UnityProjects/BuggerBall/Assets/Entities/Player/PlayerHandler.cs
@@ -5,62 +5,68 @@ using UnityEngine;
 public class PlayerHandler : MonoBehaviour {
 
     public GameObject ball;
-    public GameObject arrow;
+    public DirectionManager directionManager;
 
     private Rigidbody2D rigidBody;
 
-    private float maxJumpPower = 10f;
-    private float minJumpPower = 1f;
-    private float currentJumpPower = 1f;
-    private float jumpPowerGrowthSpeed = 5f;
+    //public float maxJumpPower = 10f;
+    //public float minJumpPower = 1f;
+    //public float currentJumpPower = 1f;
+    //public float jumpPowerGrowthSpeed = 5f;
+    public bool isStuck;
+
+    public bool CanJump
+    {
+        get
+        {
+            return isStuck;
+        }
+    }
 
     // Use this for initialization
     void Start () {
+        directionManager = GetComponentInChildren<DirectionManager>();
         rigidBody = GetComponent<Rigidbody2D>();
-        arrow.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //Lock rotation
         transform.rotation = Quaternion.identity;
 
         var mousePosition = Input.mousePosition;
-
         var ballPosition = ball.transform.position;
-
         var pos = Camera.main.WorldToScreenPoint(transform.position);
         var dir = Input.mousePosition - pos;
         var angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) - 90;
-        arrow.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        directionManager.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
         if(Input.GetMouseButton(0) && isStuck)
         {
-            if (currentJumpPower < maxJumpPower)
-                currentJumpPower += jumpPowerGrowthSpeed * Time.deltaTime;
-            var currentScaleIndex = currentJumpPower / maxJumpPower;
-
-            var currentScale = arrow.transform.localScale;
-            Debug.Log(currentJumpPower);
-            Debug.Log(currentScaleIndex);
-            arrow.transform.localScale = new Vector3(currentScaleIndex, currentScaleIndex, currentScaleIndex);
+            directionManager.IncreaseCurrentJumpPower();
         }
         else if (Input.GetMouseButtonUp(0) && isStuck)
         {
-            rigidBody.velocity = dir.normalized * currentJumpPower;
+            //Reenable the gravity on the player
             rigidBody.gravityScale = 1;
+
+            //Perform the jump
+            rigidBody.velocity = dir.normalized * directionManager.GetCurrentJumpPower();
+            
             isStuck = false;
-            arrow.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-            currentJumpPower = minJumpPower;
+
+            //Reset 
+            directionManager.ResetCurrentJumpPower();
         }
         else if (isStuck)
         {
+            //Arrest movement
             rigidBody.velocity = Vector2.zero;
+
+            //Disable the gravity on the player
             rigidBody.gravityScale = 0;
         }
     }
-
-
-    private bool isStuck;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
